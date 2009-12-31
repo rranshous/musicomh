cee7b662b3740562662153144bb91a3380c0ced7
Robby Ranshous <rranshous@gmail.com>
2009-12-31 15:41:31 -0500

    still playing w/ async

--- pull_music_gigs.py
+++ pull_music_gigs.py
@@ -5,21 +5,22 @@ from music_gigs_2009_item import pull_item as get_2009_item
 
 from multiprocessing import Pool, Process, Pipe
 
+import time
+
 LISTING_BASE_URL_2009 = 'http://musicomh.com/music/gigs/'
 
 def wrap_pipe(*args,**kwargs):
     pipe = kwargs.get('pipe')
 
 
-def get_2009():
+def get_2009(pool_size=4):
     # first pull down our item data from the listing
     items = get_2009_listing(LISTING_BASE_URL_2009)
     # now we want to extend our item data by pulling the item details
     # one by one from the individual pages
-    print 'total items:',len(items)
-    pool = Pool(2)
+    start = time.time()
+    pool = Pool(pool_size)
     for i,item_data in enumerate(items):
-        print '%s %s' % (i,item_data.get('item_link_heading'))
         # pull it's page
         #item_data.update(get_2009_item(item_data.get('item_link_href')))
 
@@ -29,8 +30,8 @@ def get_2009():
 
     pool.close()
     pool.join()
+    print 'time elapsed:',(time.time() - start)
 
-    # we should now have all the info for 2009 
+    # we should now have all the info for 2009
     return items
 
-        
